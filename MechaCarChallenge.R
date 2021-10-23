a5c658c638d998b13dc732edd992c8ac4ff2a4ec
Paramo <Sergio_Paramo@Dell.com>
2021-10-23 02:00:23 -0500

    Deliv2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,3 +1,4 @@
+# Deliverable 1
 
 library(dbplyr)#Use the library() function to load the dplyr package.
 
@@ -6,6 +7,14 @@ MechaCar_mpg <- read.csv('MechaCar_mpg.csv')# Import and read in the MechaCar_mp
 lm(formula = mpg ~ vehicle_length + vehicle_weight +	spoiler_angle +	ground_clearance + AWD, data = MechaCar_mpg)
 
 summary(lm(formula = mpg ~ vehicle_length + vehicle_weight +	spoiler_angle +	ground_clearance + AWD, data = MechaCar_mpg)) #generate summary statistics
+
+#Deliverable 2
+
+Suspension_Coil <- read.csv('Suspension_Coil.csv')
+
+total_summary <- Suspension_Coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Var=var(PSI), SD=sd(PSI), .groups = NULL)
+
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot )%>%  summarize(Mean=mean(PSI), Median=median(PSI), Var=var(PSI), SD=sd(PSI), .groups = NULL)
 
 
 
